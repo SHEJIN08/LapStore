f2d965e0311e7b0e3883ddf55baafa62bf5c1b42
SHEJIN08 <mohammedshejin08@gmail.com>
2025-11-14 09:53:26 +0530

    userSchema updated

--- model/userMode.js
+++ model/userMode.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose";
+ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase:true, trim:true },
   password: { type: String, required: true },
   avatar: { type: String,default:'user'},
-  isBlocked: {type: String, default: false},
+  refferalCode: {type: String, required:optional},
+  isActive: {type: String, default: true},
   role: { type:String, default: 'User'}
 },
  {timestamps:true}
